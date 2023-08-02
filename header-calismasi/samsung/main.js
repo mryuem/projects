f759b5302fded538d0475b8f01f95dbb48bf5b8e
mryuem <yunusemrealtunn.0@gmail.com>
2023-08-02 19:32:03 +0300

    projects
    web-site

---
+++ header-calismasi/samsung/main.js
@@ -0,0 +1,5 @@
+const openHamburger = document.getElementsByClassName(".open-hamburger");
+const Hamburgericon = document.getElementById("hamburgerMenu");
+Hamburgericon.addEventListener("click", function () {
+  console.log("a");
+});
